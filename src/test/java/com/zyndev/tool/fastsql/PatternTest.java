1a3fbce74a0f3a4d8cba96fc057f07d783e02722
zyndev <zyndev@gmail.com>
2018-02-22 14:06:33 +0800

    添加返回主键id的注解

---
+++ src/test/java/com/zyndev/tool/fastsql/PatternTest.java
@@ -0,0 +1,25 @@
+package com.zyndev.tool.fastsql;
+
+import org.junit.Test;
+
+/**
+ * 这里应该有描述
+ *
+ * @author 张瑀楠 wb.yunan.zhang@renren-inc.com
+ * @version 1.0
+ * @date 2018/2/22 13:59
+ * TODO:
+ */
+public class PatternTest {
+
+    @Test
+    public void test() {
+        String sql = "Select aa, bb ,cc dd, ee, ff from table where 1 = 1 and status != 2 " +
+                " #if(:name) { and accessory_name like concat('%', :name ,'%') } " +
+                " #if(:typeId != null ){ and type_id = :typeId  } " +
+                " #if(:status >= 0 ) { and status =:status } " +
+                " #if(:pageNum > -1 ){ limit :pageNum , :pageSize";
+        System.out.println(sql.replaceAll("(?i)select([\\s\\S]*?)from","select count(*) from "));
+    }
+
+}
